f80c00efb95be4d989bf937cadd240ec31b2d032
alexeispirit <alexeispirit@gmail.com>
2019-08-23 12:31:43 +0500

    RoomContext

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import { RoomProvider } from "./Context";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById("root")
 );
 
